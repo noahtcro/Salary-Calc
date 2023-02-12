0ecd084078feae3b9d49181cc5c35d925f9c10c5
Noah Cronin <118322736+noahtcro@users.noreply.github.com>
2023-02-12 09:21:03 -0600

    v1.1.0
    Created functions to better organize the different processes within the system.
    
    Salary and wage calculations are now their own functions. The different conversions also use their own functions.
    
    Ex. salary() for salary options and sWage() for salary to wage calculation.

--- Salary Calculator/Source.cpp
+++ Salary Calculator/Source.cpp
@@ -2,20 +2,16 @@
 #include <cstdlib> //mathmatical functions
 
 void salary();
-double sMonthly(double salary);
-double sWage(double salary);
+void sMonthly();
+void sWage();
 void wage();
-double wMonthly(double wage);
-double wAnnual(double wage);
+void wMonthly();
+void wAnnual();
 
 int main() {
 	int menu = 0;
-	double salary = 0;
-	double wage = 0;
 
 	do {
-
-
 		std::cout << "What does the position offer?" << std::endl;
 		std::cout << "1. Salary" << std::endl;
 		std::cout << "2. Wage" << std::endl;
@@ -24,97 +20,111 @@ int main() {
 
 		switch (menu) {
 		case 1:
-			system("cls"); //clear screen
-			std::cout << "How would you like to convert the salary?" << std::endl;
-			std::cout << "1. Salary -> Monthly" << std::endl;
-			std::cout << "2. Salary -> Wage" << std::endl;
-			std::cin >> menu;
-
-			switch (menu) {
-			case 1:
-				system("cls");
-				std::cout << "Please enter the annual salary: $";
-				std::cin >> salary;
-
-				salary = salary / 12;
-
-				system("cls");
-				std::cout << "The monthly income for the salary is: $" << salary << std::endl;
-				system("pause"); //enter to continue
-				system("cls");
-				
-				break;
-			case 2:
-				system("cls");
-				std::cout << "Please enter the annual salary: $";
-				std::cin >> salary;
-
-				salary = salary / 12 / 2 / 2 / 40;
-
-				system("cls");
-				std::cout << "The wage for the salary is: $" << salary << std::endl;
-				system("pause");
-				system("cls");
-				break;
-			}
+			salary();
+			break;
 		case 2:
-			system("cls");
-			std::cout << "How would you like to convert the wage?" << std::endl;
-			std::cout << "1. Wage -> Monthly" << std::endl;
-			std::cout << "2. Wage -> Annual" << std::endl;
-			std::cin >> menu;
-
-			switch (menu) {
-			case 1:
-				system("cls");
-				std::cout << "Please enter the wage: $";
-				std::cin >> wage;
-
-				wage = wage * 40 * 4;
-
-				system("cls");
-				std::cout << "The monthly income for the wage is: $" << wage << std::endl;
-				system("pause"); //enter to continue
-				system("cls");
-
-				break;
-			case 2:
-				system("cls");
-				std::cout << "Please enter the wage: $";
-				std::cin >> wage;
-
-				wage = wage * 40 * 4 * 12;
-
-				system("cls");
-				std::cout << "The annual income for the wage is: $" << wage << std::endl;
-				system("pause");
-				system("cls");
-				break;
-			}
+			wage();
+			break;
+		case 3:
+			break;
 		}
 	} while (menu != 3);
 }
 
 void salary() {
+	system("cls"); //clear screen
+	int menu = 0;
 
+	std::cout << "How would you like to convert the salary?" << std::endl;
+	std::cout << "1. Salary -> Monthly" << std::endl;
+	std::cout << "2. Salary -> Wage" << std::endl;
+	std::cin >> menu;
+
+	switch (menu) {
+	case 1:
+		sMonthly();
+		break;
+	case 2:
+		sWage();
+		break;
+	}
 }
 
-double sMonthly(double salary) {
+void sMonthly() {
+	system("cls");
+	double income = 0;
 
+	std::cout << "Please enter the annual salary: $";
+	std::cin >> income;
+
+	income = income / 12;
+
+	system("cls");
+	std::cout << "The monthly income for the salary is: $" << income << std::endl;
+	system("pause"); //enter to continue
+	system("cls");
 }
 
-double sWage(double salary) {
+void sWage() {
+	system("cls");
+	double income = 0;
 
+	std::cout << "Please enter the annual salary: $";
+	std::cin >> income;
+
+	income = income / 12 / 2 / 2 / 40;
+
+	system("cls");
+	std::cout << "The wage for the salary is: $" << salary << std::endl;
+	system("pause");
+	system("cls");
 }
 
 void wage() {
+	system("cls");
+	int menu = 0;
 
+	std::cout << "How would you like to convert the wage?" << std::endl;
+	std::cout << "1. Wage -> Monthly" << std::endl;
+	std::cout << "2. Wage -> Annual" << std::endl;
+	std::cin >> menu;
+
+	switch (menu) {
+	case 1:
+		wMonthly();
+		break;
+	case 2:
+		wAnnual();
+		break;
+	}
 }
 
-double wMonthly(double wage) {
+void wMonthly() {
+	system("cls");
+	double wage = 0;
 
+	std::cout << "Please enter the wage: $";
+	std::cin >> wage;
+
+	wage = wage * 40 * 4;
+
+	system("cls");
+	std::cout << "The monthly income for the wage is: $" << wage << std::endl;
+	system("pause"); //enter to continue
+	system("cls");
 }
 
-double wAnnual(double wage) {
+void wAnnual() {
+	system("cls");
+	double wage = 0;
 
+	std::cout << "Please enter the wage: $";
+	std::cin >> wage;
+
+	wage = wage * 40 * 4 * 12;
+
+	system("cls");
+	std::cout << "The annual income for the wage is: $" << wage << std::endl;
+	system("pause");
+	system("cls");
 }
